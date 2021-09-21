6e379507eafb04ad5e8ed824d985c49377882d2e
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2021-09-21 10:50:02 +0200

    In the beginning...

---
+++ README.md
@@ -0,0 +1,28 @@
+This repository contains a basic glossary CRUD Web API implemented in ASP.NET and secured with [Auth0](https://auth0.com/).
+
+## To run this application:
+
+1. Clone the repo with the following command: 
+
+   ```bash
+   git clone --branch starter --single-branch https://github.com/auth0-blog/glossary-rbac-web-api-aspnet.git
+   ```
+
+2. Move to the `glossary-rbac-web-api-aspnet` folder 
+
+3. Add your Auth0 domain, API identifier, Client Id and Client Secret to the `appsettings.json` configuration file (see [Registering the Web API with Auth0](https://auth0.com/blog/aspnet-web-api-authorization/#Registering-the-API-application-with-Auth0) for more details).
+
+4. Type `dotnet run` in a terminal window to launch the Web API.
+
+5. Point your browser to `https://localhost:5001/swagger` to test the available endpoints and actions.
+
+![webapi-swagger-ui](https://images.ctfassets.net/23aumh6u8s0i/1rNBChKdbCr9fKdpbbYcKR/014f6ace0411febc726959cd88ed8e6a/protected-swagger-ui-webapi.png)
+
+6. To call a protected endpoint, you need to click the Authorize button. You will be asked to log in to Auth0 to get an access token and call the API endpoint:
+   ![swagger-client-authorization](images/swagger-client-authorization.png)
+
+
+## Requirements:
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download/dotnet/current) installed on your machine
+
---
+++ Startup.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Collections.Generic;
+
+namespace Glossary
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(options =>
+      {
+        options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+        options.Audience = Configuration["Auth0:Audience"];
+      });
+
+      services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
+
+        c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+          Type = SecuritySchemeType.OAuth2,
+          Flows = new OpenApiOAuthFlows()
+          {
+            Implicit = new OpenApiOAuthFlow()
+            {
+              AuthorizationUrl = new Uri($"https://{Configuration["Auth0:Domain"]}/authorize"),
+              TokenUrl = new Uri($"https://{Configuration["Auth0:Domain"]}/token")
+            }
+          }
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+          {
+            new OpenApiSecurityScheme
+            {
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "oauth2"
+              },
+              Scheme = "oauth2",
+              Name = "oauth2",
+              In = ParameterLocation.Header
+            },
+            new List<string>()
+          }
+        });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Glossary v1");
+
+          c.OAuthClientId(Configuration["Auth0:ClientId"]);
+          c.OAuthClientSecret(Configuration["Auth0:ClientSecret"]);
+          c.OAuthAppName("GlossaryClient");
+          c.OAuthAdditionalQueryStringParams(new Dictionary<string, string> { { "audience", Configuration["Auth0:Audience"] } }); 
+          c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+        });
+      }
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
